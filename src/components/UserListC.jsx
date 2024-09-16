7a54490e365cf3fb1e5480ed74ed64a6a58bae3e
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-09-16 16:18:11 +0300

    without rdux toolkit thunk

--- src/components/UserListC.jsx
+++ src/components/UserListC.jsx
@@ -1,13 +1,22 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setError, setLoading, setUser } from "../redux/slices/classicSlice";
 
 const UserListC = () => {
+  const state = useSelector((store) => store.classicReducer);
+  const dispatch = useDispatch();
+
   useEffect(() => {
+    dispatch(setLoading());
     axios
       .get("https://dummyjson.com/users")
-      .then((res) => console.log(res.data.users))
-      .catch((err) => console.log(err));
-  });
+      .then((res) => dispatch(setUser(res.data.users)))
+      .catch((err) => dispatch(setError(err.message)));
+  }, []);
+
+  console.log(state);
+
   return <div>User</div>;
 };
 
