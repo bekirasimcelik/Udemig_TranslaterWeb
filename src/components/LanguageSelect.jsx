87a896f1ed247f1643de6a274c94ce84cd27006e
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-09-18 22:55:32 +0300

    useMemo implemented to the reactSelect

--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
+import ReactSelect from "react-select";
 
 const LanguageSelect = () => {
   const { isLoading, error, langs } = useSelector((store) => store.lang);
+
+  const formatted = useMemo(
+    () =>
+      langs.map((i) => ({
+        label: i.name,
+        value: i.code,
+      })),
+    [langs]
+  );
+
   return (
     <div className="flex gap-2 text-black">
-      <select className="flex-1">
-        {langs.map((i) => (
-          <option>{i.name}</option>
-        ))}
-      </select>
+      <ReactSelect className="flex-1" options={formatted} />
 
       <button className="bg-zinc-700 py-2 px-6 hover:bg-zinc-600 transition rounded text-white">
         Change
       </button>
 
-      <select className="flex-1">
-        <option value="">Seçiniz</option>
-      </select>
+      <ReactSelect className="flex-1" options={formatted} />
     </div>
   );
 };
